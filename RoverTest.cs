096f23741b7afb7f2841ec6c781061825072fbac
velianarie <39675129+velianarie@users.noreply.github.com>
2018-05-27 18:36:28 +0100

    Refactor tests to use NUnit 3 TestCaseSource

--- RoverTest.cs
+++ RoverTest.cs
@@ -1,11 +1,54 @@
 ﻿namespace PlutoRover
 {
     using System;
+    using System.Collections.Generic;
     using NUnit.Framework;
 
     [TestFixture]
     public class RoverTest
     {
+        private static IEnumerable<TestCaseData> OrientationCaseInsensitiveTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new Rover(0, 0, 'n'), new Position(0, 0, 'n'));
+                yield return new TestCaseData(new Rover(0, 0, 's'), new Position(0, 0, 's'));
+                yield return new TestCaseData(new Rover(0, 0, 'e'), new Position(0, 0, 'e'));
+                yield return new TestCaseData(new Rover(0, 0, 'w'), new Position(0, 0, 'w'));
+            }
+        }
+
+        private static IEnumerable<TestCaseData> CommandCaseInsensitiveTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new Rover(5, 5, 'N'), "F", new Position(5, 6, 'N'));
+                yield return new TestCaseData(new Rover(5, 5, 'N'), "f", new Position(5, 6, 'N'));
+            }
+        }
+
+        private static IEnumerable<TestCaseData> SingleCommandFTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new Rover(5, 5, 'N'), new Position(5, 6, 'N'));
+                yield return new TestCaseData(new Rover(5, 5, 'S'), new Position(5, 4, 'S'));
+                yield return new TestCaseData(new Rover(5, 5, 'E'), new Position(6, 5, 'E'));
+                yield return new TestCaseData(new Rover(5, 5, 'W'), new Position(4, 5, 'W'));
+            }
+        }
+
+        private static IEnumerable<TestCaseData> SingleCommandBTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new Rover(5, 5, 'N'), new Position(5, 4, 'N'));
+                yield return new TestCaseData(new Rover(5, 5, 'S'), new Position(5, 6, 'S'));
+                yield return new TestCaseData(new Rover(5, 5, 'E'), new Position(4, 5, 'E'));
+                yield return new TestCaseData(new Rover(5, 5, 'W'), new Position(6, 5, 'W'));
+            }
+        }
+
         [Test]
         public void NewRoverInstanceShouldSetItsPosition()
         {
@@ -14,23 +57,9 @@
             Assert.That(rover.Position, Is.EqualTo(expectedPosition));
         }
 
-        [Test]
-        public void OrientationIsCaseInsensitive()
+        [Test, TestCaseSource(nameof(OrientationCaseInsensitiveTestData))]
+        public void OrientationIsCaseInsensitive(Rover rover, Position expectedPosition)
         {
-            var rover = new Rover(0, 0, 'n');
-            var expectedPosition = new Position(0, 0, 'n');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(0, 0, 's');
-            expectedPosition = new Position(0, 0, 's');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(0, 0, 'e');
-            expectedPosition = new Position(0, 0, 'e');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(0, 0, 'w');
-            expectedPosition = new Position(0, 0, 'w');
             Assert.That(rover.Position, Is.EqualTo(expectedPosition));
         }
 
@@ -40,66 +69,24 @@
             Assert.Throws<ArgumentException>(() => new Rover(0, 0, 'x')).Message.Equals("Orientation 'x' is not valid.");
         }
 
-        [Test]
-        public void SingleCommandFShouldMoveRoverForwardByOneGridPoint()
+        [Test, TestCaseSource(nameof(SingleCommandFTestData))]
+        public void SingleCommandFShouldMoveRoverForwardByOneGridPoint(Rover rover, Position expectedPosition)
         {
-            var rover = new Rover(5, 5, 'N');
             rover.Move("F");
-            var expectedPosition = new Position(5, 6, 'N');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(5, 5, 'S');
-            rover.Move("F");
-            expectedPosition = new Position(5, 4, 'S');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(5, 5, 'E');
-            rover.Move("F");
-            expectedPosition = new Position(6, 5, 'E');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(5, 5, 'W');
-            rover.Move("F");
-            expectedPosition = new Position(4, 5, 'W');
             Assert.That(rover.Position, Is.EqualTo(expectedPosition));
         }
 
-
-        [Test]
-        public void SingleCommandBShouldMoveRoverBackwardByOneGridPoint()
+        [Test, TestCaseSource(nameof(SingleCommandBTestData))]
+        public void SingleCommandBShouldMoveRoverBackwardByOneGridPoint(Rover rover, Position expectedPosition)
         {
-            var rover = new Rover(5, 5, 'N');
             rover.Move("B");
-            var expectedPosition = new Position(5, 4, 'N');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(5, 5, 'S');
-            rover.Move("B");
-            expectedPosition = new Position(5, 6, 'S');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(5, 5, 'E');
-            rover.Move("B");
-            expectedPosition = new Position(4, 5, 'E');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(5, 5, 'W');
-            rover.Move("B");
-            expectedPosition = new Position(6, 5, 'W');
             Assert.That(rover.Position, Is.EqualTo(expectedPosition));
         }
 
-        [Test]
-        public void CommandIsCaseInsensitive()
+        [Test, TestCaseSource(nameof(CommandCaseInsensitiveTestData))]
+        public void CommandIsCaseInsensitive(Rover rover, string commands, Position expectedPosition)
         {
-            var rover = new Rover(5, 5, 'N');
-            rover.Move("F");
-            var expectedPosition = new Position(5, 6, 'N');
-            Assert.That(rover.Position, Is.EqualTo(expectedPosition));
-
-            rover = new Rover(5, 5, 'N');
-            rover.Move("f");
-            expectedPosition = new Position(5, 6, 'N');
+            rover.Move(commands);
             Assert.That(rover.Position, Is.EqualTo(expectedPosition));
         }
 
